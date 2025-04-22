d7261339c9f7f421ef3732bc55b12315e7becda8
Kubai James Karomo <129506433+kj728@users.noreply.github.com>
2025-04-22 14:25:47 +0300

    login user endpoint did some refactoring for easy reading and tested it. now users can log in using email or password.

--- backend/src/api-v1/controllers/user-basic.controllers.ts
+++ backend/src/api-v1/controllers/user-basic.controllers.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
-import { registerSchema } from "../validators/user-basic.validators";
+import { loginEmailSchema, loginUsernameSchema, registerSchema } from "../validators/user-basic.validators";
 import { v4 as uid } from 'uuid';
 import mysql from 'mysql2/promise';
 import { sqlConfig } from "../../config";
 import { Users } from "../models/user.model";
+import { error } from "console";
+import { sqlError } from "../models/user-basic.models";
 
 const pool = mysql.createPool(sqlConfig)
 
@@ -48,11 +50,100 @@ export const registerUser = async (request: Request, response: Response): Promis
     const User = rows2 as Array<Users>
     response.status(201).json({
       message: `Congratulations ${User[0].username}! You have successfully been registered on the system.`,
+
     })
 
-  } catch (error) {
+  } catch (error: sqlError | any) {
     console.error("Error registering user:", error)
-    response.status(500).json({ message: "Internal server error" })
+    response.status(500).json({ message: `An error occured: ` + error.sqlMessage })
 
   }
+}
+
+export const getUser = async (request: Request, response: Response): Promise<void> => { }
+
+export const updateUser = async (request: Request, response: Response): Promise<void> => { }
+
+export const deleteUser = async (request: Request, response: Response): Promise<void> => { }
+
+export const loginUser = async (request: Request, response: Response): Promise<void> => {
+  const { emailOrUsername, password } = request.body
+  const emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/
+  const connection = await pool.getConnection()
+
+
+  try {
+
+    //Email login
+    if (emailRegex.test(emailOrUsername)) {
+      //validate the data
+      const { error } = loginEmailSchema.validate(request.body)
+      if (error) {
+        console.error("Validation error:", error.details[0].message)
+        response.status(400).json({ message: error.details[0].message })
+        return
+      }
+      //get the user from the database
+      const [rows, fields] = await connection.query(
+        `SELECT * FROM userBasicInfo WHERE email='${emailOrUsername}' AND isDeleted=0;`
+      )
+      const User = rows as Array<Users>
+
+      //check if the user exists
+      if (User.length === 0) {
+        response.status(401).json({ message: `Oops! User does not exist. Try a different email/username?` })
+        return
+      }
+
+      //if user exists compare the saved password with the one provided
+      if (User[0].password !== password) {
+        response.status(401).json({ error: `Oh no. Looks like the passwords do not match, try again?` })
+        return
+      }
+      //if the password is correct, return the user's username
+      response.status(200).json({
+        message: `Welcome back ${User[0].username}!`,
+      })
+    }
+
+    //Username login
+    if (!emailRegex.test(emailOrUsername)) {
+
+      //validate the data
+      const { error } = loginUsernameSchema.validate(request.body)
+      if (error) {
+        console.error("Validation error:", error.details[0].message)
+        response.status(400).json({ message: error.details[0].message })
+        return
+      }
+
+      //get the user from the database
+      const [rows, fields] = await connection.query(
+        `SELECT * FROM userBasicInfo WHERE username='${emailOrUsername}' AND isDeleted=0;`
+      )
+      const User = rows as Array<Users>
+
+      //check if the user exists
+      if (User.length === 0) {
+        response.status(401).json({ error: `Oops! User does not exist. Try a different email/username?` })
+        return
+      }
+
+      //if user exists compare the saved password with the one provided 
+      if (User[0].password !== password) {
+        response.status(401).json({ error: `Oh no. Looks like the passwords do not match, try again?` })
+        return
+      }
+      //if the password is correct, return the user's username
+      response.status(200).json({
+        message: `Welcome back ${User[0].username}!`,
+      })
+    }
+
+  } catch (error: sqlError | any) {
+    console.error("Error registering user:", error)
+    response.status(500).json({ message: `An error occured: ` + error.sqlMessage })
+
+  }
+
 }
--- backend/src/api-v1/routes/user-basic.routes.ts
+++ backend/src/api-v1/routes/user-basic.routes.ts
@@ -1,8 +1,9 @@
 
 import express from 'express';
-import { registerUser } from '../controllers/user-basic.controllers';
+import { loginUser, registerUser } from '../controllers/user-basic.controllers';
 
 export const authRouter = express.Router()
 
 
-authRouter.post('/register',registerUser)
+authRouter.post('/register', registerUser)
+authRouter.post('/login', loginUser)
